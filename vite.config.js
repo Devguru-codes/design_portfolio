e3772c080acd9444661f9b81e73fd8bbd9c8f1cd
Devguru <bt23csd060@iiitn.ac.in>
2026-07-01 12:19:59 +0530

    fix: add project 4 and 5 to vite.config.js rollup options to fix 404 error

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
         project1: resolve(__dirname, 'project_1.html'),
         project2: resolve(__dirname, 'project_2.html'),
         project3: resolve(__dirname, 'project_3.html'),
+        project4: resolve(__dirname, 'project_4.html'),
+        project5: resolve(__dirname, 'project_5.html'),
       },
     },
   },
